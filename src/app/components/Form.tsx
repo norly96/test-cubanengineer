ef0b7f46dd9dddbe927ed3998088629af8946fbe
Norluis Galvez <norly021196@gmail.com>
2024-03-06 18:09:05 -0500

    add component Form

---
+++ src/app/components/Form.tsx
@@ -0,0 +1,133 @@
+"use client";
+import Image from "next/image";
+import { useState } from "react";
+
+const Form = () => {
+  const [show, setShow] = useState<boolean>(false);
+  const [value, setValue] = useState<string>("");
+
+  const showActions = () => {
+    setShow(true);
+  };
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  };
+
+  const formatText = (value: string) => {
+    const mentionRegex = /@\w+/g;
+    const emailRegex = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b/g;
+    const hashtagRegex = /#\w+/g;
+    const urlRegex = /www\.[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}/g;
+   
+    const parts = value.split(/(\s+)/);
+   
+    return parts.map((part, index) => {
+       if (part.match(emailRegex)) {
+         return <span key={index} className="text-amber-500">{part}</span>;
+       } else if (part.match(hashtagRegex)) {
+         return <span key={index} className="text-violet-800">{part}</span>;
+       } else if (part.match(urlRegex)) {
+         return <span key={index} className="text-blue-600">{part}</span>;
+       } 
+       else if (part.match(mentionRegex)) {
+        return <span key={index} className="text-emerald-600">{part}</span>;
+      } 
+      else {
+         return <span key={index}>{part}</span>;
+       }
+    });
+   };
+
+  return (
+    <div>
+      <div onClick={showActions} className="flex flex-row  h-10 mx-6 mt-10">
+        <Image
+          src="/plus-square.svg"
+          width={24}
+          height={24}
+          alt="plus-square"
+        />
+        <div className="relative flex items-center mx-2 w-full">
+          <input
+            type="text"
+            value={value}
+            className="absolute items-center focus:outline-none h-6 w-full "
+            onChange={handleChange}
+            placeholder="Type to add new task"
+          />
+          <div className="absolute w-full h-6 pointer-events-none">
+          {formatText(value)}
+          </div>
+        </div>
+      </div>
+
+      {show && (
+        <div className="flex justify-between mx-6 pb-2 px-1 shadow-md rounded-sm">
+          <div className="flex flex-row gap-1">
+            <button className="flex flex-row mr-3 px-3 py-1 bg-gray-200 border-2 rounded-lg">
+              <Image
+                src="/maximize-2.svg"
+                width={20}
+                height={20}
+                alt="maximize"
+              />
+              <span className="ml-2 text-sm font-semibold">Open</span>
+            </button>
+            <button className="flex flex-row px-3 py-1 border-gray-500 border-2 rounded-lg">
+              {" "}
+              <Image
+                src="/calendar.svg"
+                width={20}
+                height={20}
+                alt="calendar"
+              />
+              <span className="ml-2 text-sm font-semibold">Today</span>
+            </button>
+            <button className="flex flex-row px-3 py-1 border-gray-500 border-2 rounded-lg" >
+              {" "}
+              <Image
+                src="/unlock.svg"
+                width={20}
+                height={20}
+                alt="unlock"
+              />
+              <span className="ml-2 text-sm font-semibold">Public</span>
+            </button>
+            <button className="flex flex-row px-3 py-1 border-gray-500 border-2 rounded-lg">
+              {" "}
+              <Image
+                src="/loader.svg"
+                width={20}
+                height={20}
+                alt="loader"
+              />
+              <span className="ml-2 text-sm font-semibold">Highlight</span>
+            </button>
+            <button className="flex flex-row px-3 py-1 border-gray-500 border-2 rounded-lg">
+              {" "}
+              <Image
+                src="/disc.svg"
+                width={20}
+                height={20}
+                alt="disc"
+              />
+              <span className="ml-2 text-sm font-semibold">Estimation</span>
+            </button>
+          </div>
+          <div>
+            <button onClick={() => setShow(false)} className="mr-1 px-4 py-1 bg-gray-200 border-2 rounded-lg">
+              <span className="text-sm text-black font-semibold">Cancel</span>
+            </button>
+            <button className="mr-1 px-4 py-1 bg-blue-600 rounded-lg">
+              <span className="text-sm text-white font-semibold">Ok</span>
+            </button>
+          </div>
+        </div>
+      )}
+
+      <div></div>
+    </div>
+  );
+};
+
+export default Form;
